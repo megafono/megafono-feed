491a78343eed6df320cc6bbcea9448c3f91e5915
Emerson "Duke" Almeida <duke.m16@gmail.com>
2017-10-29 20:22:44 -0200

    build URL from hash

--- src/megafono_feed/views/helpers/url.clj
+++ src/megafono_feed/views/helpers/url.clj
@@ -1,7 +1,13 @@
-(ns megafono-feed.views.helpers.url)
+(ns megafono-feed.views.helpers.url
+  (:require [hashids.core :as id]))
+
+(def hashids-opts {:salt "MEGAFONO SHORTEN"})
 
 (defn build-site-url [slug]
   (apply str ["https://www.megafono.io/podcast/" slug]))
+
+(defn build-episode-url [episode]
+  (apply str ["http://mfn.bz/" (id/encode hashids-opts (:nid episode))]))
 
 (defn buid-xml-url [slug]
   (apply str ["https://feed.megafono.io/" slug]))
