228a62cc25fb0e13aa244e821ba559cb123cd9c8
Emerson "Duke" Almeida <duke.m16@gmail.com>
2017-11-13 08:59:14 -0200

    improve index

--- src/megafono_feed/models/channel.clj
+++ src/megafono_feed/models/channel.clj
@@ -13,21 +13,27 @@
 (use 'korma.core)
 
 (def activated_channel (-> (select* channels)
+                           (modifier "DISTINCT")
                            (join :inner slugs (and
-                                                (= :friendly_id_slugs.sluggable_id :id)
-                                                (= :friendly_id_slugs.sluggable_type "Channel")))
-                           (with categories)
-                           (with episodes
-                             (where {:media_status "uploaded"})
-                             (where (< :published_at (c/to-sql-time (java.util.Date.))))
-                             (order :published_at :DESC))
-                           (with channel_ownerships
-                             (where {:ownerable_type "User"
-                                     :level [in [0 1]]})
-                             (limit 1)
-                             (with users))
+                                               (= :friendly_id_slugs.sluggable_id :id)
+                                               (= :friendly_id_slugs.sluggable_type "Channel")))
                            (where {:deleted_at nil
-                                   :status [not= "pending"]})))
+                                   :status [not= "pending"]})
+                           (order :name)))
+
+
+(def activated_channel_with_relashionship (-> (select* activated_channel)
+                                                   (with categories)
+                                                   (with episodes
+                                                         (where {:media_status "uploaded"})
+                                                         (where (< :published_at (c/to-sql-time (java.util.Date.))))
+                                                         (order :published_at :DESC))
+                                                   (with channel_ownerships
+                                                         (where {:ownerable_type "User"
+                                                                 :level [in [0 1]]})
+                                                         (limit 1)
+                                                         (with users))))
+
 
 (defn all []
   (into [] (select activated_channel
@@ -35,6 +41,6 @@
 
 (defn find-by-slug [slug]
   (first
-    (select activated_channel
-            (where {:friendly_id_slugs.slug slug})
-            (limit 1))))
+   (select activated_channel_with_relashionship
+           (where {:friendly_id_slugs.slug slug})
+           (limit 1))))
